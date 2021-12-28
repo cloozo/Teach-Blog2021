dcb958750486a864d6463c389968b00eba7700d9
cloozo <mybenefitchoiceinc@gmail.com>
2021-12-28 14:57:25 -0500

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+// create connection to our db
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 8889,
+  }
+);
+
+module.exports = sequelize;
